696eaf0aa3005070a9149d3c7ca1021775e1f308
MrJaguar <guajardo_luis@outlook.com>
2016-10-29 04:26:47 -0500

    Beta 1

--- app/src/main/java/com/example/oscar/apptest/MainActivity.java
+++ app/src/main/java/com/example/oscar/apptest/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.oscar.apptest;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -13,6 +17,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final int CAMERA_REQUEST= 1;
     private Button photo;
     private ImageView view;;
+    private final int PHOTO_CODE = 200;
+    private final int SELECT_PICTURE = 300;
+
+    private ImageView mSetImage;
+    private Button mOptionButton;
+    private String mPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         photo.setOnClickListener(this);
 
         view=(ImageView) findViewById(R.id.image);
+
+
+        mSetImage = (ImageView) findViewById(R.id.image);
+        mOptionButton = (Button) findViewById(R.id.show_options_button);
+
+
+        mOptionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // showOptions();
+                Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(intent.createChooser(intent, "Selecciona app de imagen"), SELECT_PICTURE);
+            }
+        });
     }
 
     @Override
@@ -35,12 +60,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == CAMERA_REQUEST && resultCode == RESULT_OK){
+        /*if(requestCode == CAMERA_REQUEST && resultCode == RESULT_OK){
 
             Bitmap photo=(Bitmap) data.getExtras().get("data");
             view.setImageBitmap(photo);
 
+        }*/if(resultCode == RESULT_OK){
+            switch (requestCode){
+                case CAMERA_REQUEST:
+                    MediaScannerConnection.scanFile(this,
+                            new String[]{mPath}, null,
+                            new MediaScannerConnection.OnScanCompletedListener() {
+                                @Override
+                                public void onScanCompleted(String path, Uri uri) {
+                                    Log.i("ExternalStorage", "Scanned " + path + ":");
+                                    Log.i("ExternalStorage", "-> Uri = " + uri);
+                                }
+                            });
+
+
+                    Bitmap bitmap = BitmapFactory.decodeFile(mPath);
+                    mSetImage.setImageBitmap(bitmap);
+                    break;
+                case SELECT_PICTURE:
+                    Uri path = data.getData();
+                    mSetImage.setImageURI(path);
+                    break;
+
+            }
         }
+
 
     }
 }
